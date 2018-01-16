3a8c10d77f8982ca0ff32f06fffe8a3019db3186
Gillis Robbe <robbe.gillis@immalle.eu>
2018-01-16 09:33:21 +0100

    Add project files.

---
+++ SwapGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SwapGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
