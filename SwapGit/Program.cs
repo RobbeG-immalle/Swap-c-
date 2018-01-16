8ea1f749b57967593131e74f5245dc22381ab4e2
robbe.gillis <robbe.gillis@B209-11>
2018-01-16 09:34:53 +0100

    Done

--- SwapGit/Program.cs
+++ SwapGit/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 
-namespace SwapGit
+namespace Swap
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int x = 5;
+            int y = 10;
+            Swap(ref x, ref y);
+        }
+        static void Swap(ref int getal1, ref int getal2)
+        {
+            int tempSwap = getal1;
+            getal1 = getal2;
+            getal2 = tempSwap;
+            Console.WriteLine("{0} is nu getal1, {1} is nu getal2", getal1, getal2);
         }
     }
 }
